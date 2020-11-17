4ae336928f323884091687dd1d76f14b44c3f224
Osman <frost57@gmail.com>
2020-11-17 00:45:13 -0500

    completed project

--- main.cpp
+++ main.cpp
@@ -1,42 +1,57 @@
+/*
+ * Osman Momoh
+ * COMP 6331 : Distributed Systems
+ * Assignment 4 : Parallel Matrix Multiplication using MPI
+ * Fall Semester 2020
+ */
+
 #include<stdlib.h>
 #include<time.h>
 #include<mpi.h>
 
-#define ROWSA 2000      //Rows in left-sided matrix A
+#define ROWSA 500       //Rows in left-sided matrix A
 #define COLSA 32        //Columns in left-sided matrix A
 #define ROWSB 32        //Rows in right-sided matrix B
-#define COLSB 2000      //Columns in right sided matrix B
-#define ROWSC 2000      //Rows in result matrix C
-#define COLSC 2000      //Columns in result matrix C
+#define COLSB 500       //Columns in right sided matrix B
+#define ROWSC 500       //Rows in result matrix C
+#define COLSC 500       //Columns in result matrix C
 #define MASTER 0        //ID of master task
 #define MASTERTAG 1     //Tag of master task
 #define WORKERTAG 2     //Tag of worker task
 
-void makeArrA(int arr[ROWSA][COLSA]);
-void makeArrB(int arr[ROWSB][COLSB]);
-void printArrA(int arr[ROWSA][COLSA]);
-void printArrB(int arr[ROWSB][COLSB]);
-void printArrC(int arr[ROWSC][COLSC], int);
-void nonParallelExec();
-void makeMatrices();
-void initMPI(int &argc, char **&argv);
-void masterTask();
-void workerTask();
-void calculateRowDistributions();
+void makeMatrices();                                //Make array A and B
+void makeArrA(int arr[ROWSA][COLSA]);               //Make array A
+void makeArrB(int arr[ROWSB][COLSB]);               //Make array B
+void printArrA(int arr[ROWSA][COLSA]);              //Print array A
+void printArrB(int arr[ROWSB][COLSB]);              //Print array B
+void printArrC(int arr[ROWSC][COLSC], int);         //Print array C
+void nonParallelExec();                             //When number of tasks is 1, perform multiplication sequentially
+void initMPI(int &argc, char **&argv);              //Start the MPI
+void masterTask();                                  //Perform the master task
+void workerTask();                                  //Perform a worker task
+void calculateRowDistributions();                   //Calculate how rows are distributed to worker tasks
+void checkInput();                                  //Checks the definitions are valid
 
-int matA[ROWSA][COLSA];
-int matB[ROWSB][COLSB];
-int matC[ROWSC][COLSC];
-int result[ROWSC][COLSC];
-int numTasks, taskID, rowsPerTask = 0, rowsWithExtra = 0, rowStart = 0, rowsToDo = 0;
-clock_t start, end;
-MPI_Status status;
+int matA[ROWSA][COLSA];                 //Matrix A
+int matB[ROWSB][COLSB];                 //Matrix B
+int matC[ROWSC][COLSC];                 //Matrix C
+int result[ROWSC][COLSC];               //The final resulting matrix
+int numTasks,                           //number of tasks
+    taskID,                             //ID of task
+    rowsPerTask = 0,                    //Rows per each task
+    tasksWithExtraRow = 0,              //Number of tasks with extra row
+    rowStart = 0,                       //Row in which a worker task should begin
+    rowsToDo = 0;                       //Number of rows a task should process
+clock_t start, end;                     //Start and end times
+MPI_Status status;                      //Status of MPI receive
 
 int main(int argc, char* argv[]) {
 
+    checkInput();
     initMPI(argc, argv);
     makeMatrices();
 
+    //If number of tasks is 1, perform sequential multiplication and terminate
     if(numTasks == 1) {
         nonParallelExec();
     }
@@ -58,29 +73,46 @@ void masterTask() {
     //printArrB(matB);
     start = clock();
 
+    //Number of integers to send to worker tasks
     int MPICountA, MPICountB;
+    //Arrays for holding which row each task started on and how many rows were processed
+    int rowStartArr[numTasks-1];
+    int rowsToDoArr[numTasks-1];
 
+    //Send messages to the workers
     for(int i = 1 ; i < numTasks ; i++) {
 
-        MPICountA = i <= rowsWithExtra ? (rowsPerTask + 1) * COLSA : rowsPerTask * COLSA;
+        //Checks if this is a task which should have an extra row. It then multiplies the number of rows *
+        //the number of columns of A, to get the number of integers to send in matrix A
+        MPICountA = i <= tasksWithExtraRow ? (rowsPerTask + 1) * COLSA : rowsPerTask * COLSA;
+        //The entire matrix B needs to be sent to each worker
         MPICountB = ROWSB * COLSB;
+        //Which row the worker should start with in A
         rowStart += rowsToDo;
+        //How many rows of A the worker should process
         rowsToDo = MPICountA / COLSA;
+        //Store these values in an array
+        rowStartArr[i-1] = rowStart;
+        rowsToDoArr[i-1] = rowsToDo;
 
+        //Send the number of rows in A to do in worker i
         MPI_Send(&rowsToDo, 1, MPI_INT, i, MASTERTAG, MPI_COMM_WORLD);
+        //Which row worker i should start on in A
         MPI_Send(&rowStart, 1, MPI_INT, i, MASTERTAG, MPI_COMM_WORLD);
+        //Send matrix A, beginning at the appropriate row, and with the right number of integers
         MPI_Send(&matA[rowStart], MPICountA, MPI_INT, i, MASTERTAG, MPI_COMM_WORLD);
+        //Send the entirety of matrix B
         MPI_Send(&matB, MPICountB, MPI_INT, i, MASTERTAG, MPI_COMM_WORLD);
     }
 
+    //Receive messages from the workers
     for(int i = 1 ; i < numTasks ; i++) {
 
-        MPI_Recv(&rowStart, 1, MPI_INT, i, WORKERTAG, MPI_COMM_WORLD, &status);
-        MPI_Recv(&rowsToDo, 1, MPI_INT, i, WORKERTAG, MPI_COMM_WORLD, &status);
-        MPI_Recv(&matC[rowStart], rowsToDo*COLSC, MPI_INT, i, WORKERTAG, MPI_COMM_WORLD, &status);
+        //Receive the array results of the worker
+        MPI_Recv(&matC[rowStartArr[i-1]], rowsToDoArr[i-1]*COLSC, MPI_INT, i, WORKERTAG, MPI_COMM_WORLD, &status);
 
-
-        for(int y = rowStart; y < rowStart + rowsToDo; y++) {
+        //Take results from worker arrays, add them to result array
+        for(int y = rowStartArr[i-1]; y < rowStartArr[i-1] + rowsToDoArr[i-1]; y++) {
             for(int x = 0; x < ROWSC; x++) {
                 result[y][x] = matC[y][x];
             }
@@ -96,13 +128,18 @@ void masterTask() {
 
 void workerTask() {
 
+    //Receive from master number of rows to do
     MPI_Recv(&rowsToDo, 1, MPI_INT, 0, MASTERTAG, MPI_COMM_WORLD, &status);
+    //Receive from master which row to start on
     MPI_Recv(&rowStart, 1, MPI_INT, 0, MASTERTAG, MPI_COMM_WORLD, &status);
+    //Receive from master matrix A at appropriate start row
     MPI_Recv(&matA[rowStart], rowsToDo*COLSA, MPI_INT, 0, MASTERTAG, MPI_COMM_WORLD, &status);
+    //Receive from master all of matrix B
     MPI_Recv(&matB, ROWSB*COLSB, MPI_INT, 0, MASTERTAG, MPI_COMM_WORLD, &status);
 
     //printf("rank %d starts at row: %d and has %d rows to do\n", taskID, rowStart, rowsToDo);
 
+    //Perform the matrix multiplication of the appropriate rows
     for(int i = rowStart; i < rowStart + rowsToDo; i++) {
         for(int j = 0; j < COLSC; j++){
 
@@ -116,11 +153,9 @@ void workerTask() {
 
     //printArrC(matC, taskID);
 
-    MPI_Send(&rowStart, 1, MPI_INT, 0, WORKERTAG, MPI_COMM_WORLD);
-    MPI_Send(&rowsToDo, 1, MPI_INT, 0, WORKERTAG, MPI_COMM_WORLD);
+    //return this work back to the master
     MPI_Send(&matC[rowStart], rowsToDo*COLSC, MPI_INT, 0, WORKERTAG, MPI_COMM_WORLD);
 }
-
 
 void nonParallelExec() {
 
@@ -153,7 +188,7 @@ void calculateRowDistributions() {
     double rowsWithExtraDouble = (ROWSA + 0.0) / (numTasks - 1);
     int intPart = (int) rowsWithExtraDouble;
     rowsWithExtraDouble -= intPart;
-    rowsWithExtra = (int)(rowsWithExtraDouble * (numTasks - 1));
+    tasksWithExtraRow = (int)(rowsWithExtraDouble * (numTasks - 1));
 }
 
 void initMPI(int &argc, char **&argv) {
@@ -225,4 +260,12 @@ void printArrC(int arr[ROWSC][COLSC], int theTaskID) {
     }
 
     printf("\n\n");
+}
+
+void checkInput() {
+
+    if((COLSA != ROWSB) || (ROWSA != ROWSC) || (COLSB != COLSC)) {
+        printf("invalid input");
+        exit(0);
+    }
 }
